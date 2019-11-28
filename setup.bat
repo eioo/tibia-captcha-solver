bd34762546e5d1865fa66299d3a1f361fa05d511
Kalle <khirvola@gmail.com>
2019-11-28 16:10:58 +0200

    Update

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python.exe  -m pip install requirements.txt
+python.exe  -m pip install -r requirements.txt
 set script_path=%cd%
 setx TESSDATA_PREFIX "%script_path%\tessdata" >nul
 echo TESSDATA_PREFIX added to environment variables
