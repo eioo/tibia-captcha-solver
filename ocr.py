864f610e33d256f29d43754a66ff9ad92b1b9dbf
Kalle <khirvola@gmail.com>
2019-11-29 07:36:50 +0200

    Update

--- ocr.py
+++ ocr.py
@@ -76,7 +76,9 @@ def solve_captcha(image):
     #  --psm 6    Assume a single uniform block of text.
     image = Image.fromarray(image)
     text = pytesseract.image_to_string(image, config=f'--psm 6')
-    text = replace_all(text, [['G', '6'], ['o', '0'], ['O', '0'], ['s', '5'], ['S', '5']])
+    text = replace_all(text,
+                       [['i', '1'], ['I', '1'], ['l', '1'], ['B', '8'], ['G', '6'], ['o', '0'], ['O', '0'], ['s', '5'],
+                        ['S', '5']])
 
     try:
         expr = replace_all(text.split('\n')[0], [[' ', ''], ['.', ''], [',', '']])
@@ -89,9 +91,14 @@ def solve_captcha(image):
         print(text)
         return False
 
-    print(text)
     time.sleep(1)
-    answer = eval(expr)
+
+    try:
+        answer = eval(expr)
+    except SyntaxError:
+        print(text)
+        return False
+
     choices = [int(n) for n in re.findall(r'\n+(-?\d+)', text)]
     print(f'{expr} = {answer}')
 
@@ -103,16 +110,20 @@ def solve_captcha(image):
 
 def click_choice(x, y, index):
     choice_x = x + 15
-    choice_y = y + 14 + index * 21
+    choice_y = y + 14 + (index + 1) * 21
     print(f'Selecting choice #{index + 1} ({choice_x}, {choice_y})')
-    pyautogui.click(choice_x, choice_y)
+    pyautogui.moveTo(choice_x, choice_y, duration=1)
+    pyautogui.click()
 
 
 def click_select(x, y):
-    pyautogui.click(x + 429, y + 172)
+    target_x = x + 429
+    target_y = y + 172
+    pyautogui.moveTo(target_x, target_y, duration=1)
+    pyautogui.click()
 
 
-def activate_captcha():
+def activate_window():
     global w
 
     try:
@@ -122,14 +133,26 @@ def activate_captcha():
         print('No archlight window found? Exiting...')
         exit(0)
 
+
+def activate_captcha():
+    activate_window()
     time.sleep(1)
-    pyautogui.typewrite('!me', interval=0.2)
+    pyautogui.typewrite('!me', interval=0.25)
+    pyautogui.typewrite(['enter'])
+
+
+def cycle_wait():
+    wait_time = random.randint(30, 45)
+    print(f'Solved captcha. Waiting for {wait_time} seconds before next solve')
+    time.sleep(wait_time)
 
 
 def main():
     print('Typing "!me"')
     activate_captcha()
     print('Searching for captcha')
+
+    start_time = int(time.time())
 
     while True:
         captcha_region = locate_captcha()
@@ -138,23 +161,36 @@ def main():
             break
 
         time.sleep(2)
+        now = int(time.time())
+
+        if now - start_time > 10:
+            print('No captcha found')
+            cycle_wait()
+            return main()
 
     print('Captcha found, trying to solve')
 
     cropped = crop_captcha(captcha_region)
+    print(f'Found captcha at: ({cropped["x"]}, {cropped["y"]})')
+
     filtered = filter_color(cropped['img'])
+    print(f'Filtered text color from captcha')
 
     choice_index = solve_captcha(filtered)
 
     if choice_index is False:
-        print('Failed to solve')
-        return
+        print('Failed to solve, trying again')
+        activate_window()
+        pyautogui.typewrite(['esc'])
+        time.sleep(1)
+        return main()
 
+    print(f'Clicking choice')
     click_choice(cropped['x'], cropped['y'], choice_index)
     time.sleep(1)
+    print(f'Clicking "select"')
     click_select(cropped['x'], cropped['y'])
-    wait_time = random.randint(30, 45)
-    print(f'Solved captcha. Waiting for {wait_time} seconds before next solve')
+    cycle_wait()
 
     main()
 
